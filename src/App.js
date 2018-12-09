8b43310b63759bff2c2ae4e0f7e42c7150d0e41a
Jooyoung Son <sjyoung0629@gmail.com>
2018-12-09 10:33:55 +0900

    181209_react-router 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Editor from './components/Editor';
-import SearchList from './components/SearchList';
-import CommentView from './components/CommentView';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/editor" component={Editor}/>
-          <Route exact path="/searchList" component={SearchList}/>
-          <Route path="/commentView" component={CommentView}/>
+          <Route path="/" component={CommentView}/>
         </div>
       </Router>
     );
